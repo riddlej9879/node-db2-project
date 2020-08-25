106a4b816b26b9ba1c6457da5fd00176b20c8309
Josh Riddle <riddlej9879@yahoo.com>
2020-08-25 18:16:31 -0500

    MVP submission

--- routers/car-router.js
+++ routers/car-router.js
@@ -34,28 +34,19 @@ router.get("/cars/:id", async (req, res, next) => {
 });
 
 router.post("/cars", async (req, res, next) => {
-  const newCar = {
-    vin: req.body.vin,
-    make: req.body.make,
-    model: req.body.model,
-    mileage: req.body.mileage,
-    transmission: req.body.transmission || null,
-    salvaged: req.body.salvaged || null,
-  };
+  try {
+    const [id] = await db("car-dealer").insert(req.body);
+    const newCar = await db("car-dealer").where({ id }).first();
 
-  db.insert(newCar)
-    .then(() => {
-      return res.status(201).json(newCar);
-    })
-    .catch((err) => {
-      return res
-        .status(500)
-        .json({ message: "Error creating new car", newCar });
-    });
+    res.status(201).json(newCar);
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
 
+//    Failed attempts
 // router.post("/cars", (req, res) => {
 //   db("cars")
 //     .insert({
@@ -71,5 +62,35 @@ module.exports = router;
 //     })
 //     .catch((err) => {
 //       res.status(500).json({ message: "Failed to create car" });
+//     });
+// });
+
+// router.post("/:id", async (req, res) => {
+//   try {
+//     const newCar = await db("cars").insert(req.body);
+//     res.status(201).json(newCar);
+//   } catch (error) {
+//     res.status(500).json({ message: "Missing data for post." });
+//   }
+// });
+
+// router.post("/cars", (req, res, next) => {
+//   const newCar = {
+//     vin: req.body.vin,
+//     make: req.body.make,
+//     model: req.body.model,
+//     mileage: req.body.mileage,
+//     transmission: req.body.transmission || null,
+//     salvaged: req.body.salvaged || null,
+//   };
+
+//   db.insert(newCar)
+//     .then(() => {
+//       return res.status(201).json(newCar);
+//     })
+//     .catch((err) => {
+//       return res
+//         .status(500)
+//         .json({ message: "Error creating new car", newCar });
 //     });
 // });
